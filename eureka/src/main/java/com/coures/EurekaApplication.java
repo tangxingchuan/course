d8eed63c730331bc0be7613284d3ef03f0dc193e
tangtao <tangrongtao69@163.com>
2022-05-18 02:52:50 +0800

    1.将system注册到eureka里面

--- eureka/src/main/java/com/coures/EurekaApplication.java
+++ eureka/src/main/java/com/coures/EurekaApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //注册服务端
 public class EurekaApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
