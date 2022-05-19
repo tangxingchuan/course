14957df04e34108c3fbff5fc7ce7e5ce1c6e5d45
tangtao <tangrongtao69@163.com>
2022-05-19 11:33:09 +0800

    1.在gateway中增加system路由转发配置。调试成功 2.修改了上一次提交gateway没办法正常启动的bug，原因是依赖导错出问题了

--- gateway/src/main/java/com/coures/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/coures/gateway/GatewayApplication.java
@@ -9,19 +9,18 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
+
 public class GatewayApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(EurekaApplication.class, args);
-//	}
+
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(GatewayApplication.class);
 		Environment env = app.run(args).getEnvironment();
 		LOG.info("启动成功！！");
-		LOG.info("Gatewayapplication.properties地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
