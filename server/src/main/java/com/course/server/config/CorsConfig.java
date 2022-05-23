f678a9d744a57b31cd03920a426c3c592b0d661d
tangtao <tangrongtao69@163.com>
2022-05-23 17:21:30 +0800

    1.解决gateway跨域问题，在启动项里加入了跨域配置 2.单个的跨域配置CorsConfig，我已经注销掉了

--- server/src/main/java/com/course/server/config/CorsConfig.java
+++ server/src/main/java/com/course/server/config/CorsConfig.java
@@ -1,13 +1,14 @@
+/*
 package com.course.server.config;
-  
+
   import org.springframework.context.annotation.Configuration;
   import org.springframework.web.cors.CorsConfiguration;
   import org.springframework.web.servlet.config.annotation.CorsRegistry;
   import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-  
+
   @Configuration
   public class CorsConfig implements WebMvcConfigurer {
-  
+
       @Override
       public void addCorsMappings(CorsRegistry registry) {
           registry.addMapping("/**")
@@ -17,5 +18,6 @@ package com.course.server.config;
                   .allowCredentials(true)
                   .maxAge(3600);    //1小时内不需要再预检（发OPTIONS请求）
       }
-  
+
   }
+*/
