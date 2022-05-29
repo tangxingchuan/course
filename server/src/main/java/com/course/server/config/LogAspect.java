d8df715a685e35c7b5f99aa09a0ea3097440e3b7
tangtao <tangrongtao69@163.com>
2022-05-30 03:09:35 +0800

    1.生成器开发：增加dto生成

--- server/src/main/java/com/course/server/config/LogAspect.java
+++ server/src/main/java/com/course/server/config/LogAspect.java
@@ -24,95 +24,91 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 
-/**
- * @author TangKe（唐柯）
- * @date 2022/5/27 - 5:45 下午
- */
-    @Aspect
-    @Component
-    public class LogAspect {
+@Aspect
+@Component
+public class LogAspect {
 
-        private final static Logger LOG = LoggerFactory.getLogger(com.course.server.config.LogAspect.class);
+    private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
 
-        /** 定义一个切点 */
-        @Pointcut("execution(public * com.course.*.controller..*Controller.*(..))")
-        public void controllerPointcut() {}
+    /** 定义一个切点 */
+    @Pointcut("execution(public * com.course.*.controller..*Controller.*(..))")
+    public void controllerPointcut() {}
 
-        @Before("controllerPointcut()")
-        public void doBefore(JoinPoint joinPoint) throws Throwable {
-            // 日志编号
-            MDC.put("UUID", UuidUtil.getShortUuid());
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 日志编号
+        MDC.put("UUID", UuidUtil.getShortUuid());
 
-            // 开始打印请求日志
-            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-            HttpServletRequest request = attributes.getRequest();
-            Signature signature = joinPoint.getSignature();
-            String name = signature.getName();
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
 
-            // 打印业务操作
-            String nameCn = "";
-            if (name.contains("list") || name.contains("query")) {
-                nameCn = "查询";
-            } else if (name.contains("save")) {
-                nameCn = "保存";
-            } else if (name.contains("delete")) {
-                nameCn = "删除";
-            } else {
-                nameCn = "操作";
-            }
-
-            // 使用反射，获取业务名称
-            Class clazz = signature.getDeclaringType();
-            Field field;
-            String businessName = "";
-            try {
-                field = clazz.getField("BUSINESS_NAME");
-                if (!StringUtils.isEmpty(field)) {
-                    businessName = (String) field.get(clazz);
-                }
-            } catch (NoSuchFieldException e) {
-                LOG.error("未获取到业务名称");
-            } catch (SecurityException e) {
-                LOG.error("获取业务名称失败", e);
-            }
-
-            // 打印请求信息
-            LOG.info("------------- 【{}】{}开始 -------------", businessName, nameCn);
-            LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
-            LOG.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
-            LOG.info("远程地址: {}", request.getRemoteAddr());
-
-            // 打印请求参数
-            Object[] args = joinPoint.getArgs();
-            Object[] arguments  = new Object[args.length];
-            for (int i = 0; i < args.length; i++) {
-                if (args[i] instanceof ServletRequest
-                        || args[i] instanceof ServletResponse
-                        || args[i] instanceof MultipartFile) {
-                    continue;
-                }
-                arguments[i] = args[i];
-            }
-            // 排除字段，敏感字段或太长的字段不显示
-            String[] excludeProperties = {"shard"};
-            PropertyPreFilters filters = new PropertyPreFilters();
-            PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
-            excludefilter.addExcludes(excludeProperties);
-            LOG.info("请求参数: {}", JSONObject.toJSONString(arguments, excludefilter)); // 为空的会不打印，但是像图片等长字段也会打印
+        // 打印业务操作
+        String nameCn = "";
+        if (name.contains("list") || name.contains("query")) {
+            nameCn = "查询";
+        } else if (name.contains("save")) {
+            nameCn = "保存";
+        } else if (name.contains("delete")) {
+            nameCn = "删除";
+        } else {
+            nameCn = "操作";
         }
 
-        @Around("controllerPointcut()")
-        public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-            long startTime = System.currentTimeMillis();
-            Object result = proceedingJoinPoint.proceed();
-            // 排除字段，敏感字段或太长的字段不显示
-            String[] excludeProperties = {"password", "shard"};
-            PropertyPreFilters filters = new PropertyPreFilters();
-            PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
-            excludefilter.addExcludes(excludeProperties);
-            LOG.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
-            LOG.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-            return result;
+        // 使用反射，获取业务名称
+        Class clazz = signature.getDeclaringType();
+        Field field;
+        String businessName = "";
+        try {
+            field = clazz.getField("BUSINESS_NAME");
+            if (!StringUtils.isEmpty(field)) {
+                businessName = (String) field.get(clazz);
+            }
+        } catch (NoSuchFieldException e) {
+            LOG.error("未获取到业务名称");
+        } catch (SecurityException e) {
+            LOG.error("获取业务名称失败", e);
         }
 
+        // 打印请求信息
+        LOG.info("------------- 【{}】{}开始 -------------", businessName, nameCn);
+        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
+        LOG.info("远程地址: {}", request.getRemoteAddr());
+
+        // 打印请求参数
+        Object[] args = joinPoint.getArgs();
+        Object[] arguments  = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // 排除字段，敏感字段或太长的字段不显示
+        String[] excludeProperties = {"shard"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("请求参数: {}", JSONObject.toJSONString(arguments, excludefilter)); // 为空的会不打印，但是像图片等长字段也会打印
     }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // 排除字段，敏感字段或太长的字段不显示
+        String[] excludeProperties = {"password", "shard"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        LOG.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+}
