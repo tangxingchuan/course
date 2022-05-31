9bbce413259661618e8aa61f7caa1a8da83093c7
tangtao <tangrongtao69@163.com>
2022-06-01 02:45:08 +0800

    1.枚举字段，表格显示中文，表单显示下拉框 2.vue中增加filter.js，过滤器，也在main.js中打开全局过滤

--- admin/src/main.js
+++ admin/src/main.js
@@ -3,12 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from "axios";
+import filter from "@/filter/filter";
 
 
 Vue.config.productionTip = false
 Vue.use(axios);
 
+// 全局过滤器
+Object.keys(filter).forEach(key => {
+    Vue.filter(key, filter[key])
+});
 
+
+/**
+ * axios拦截器
+ */
 axios.interceptors.request.use(function (config) {
     console.log('请求：',config);
     return config;
