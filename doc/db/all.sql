b9413ea4a8c8afa2d806d3152ef67ac336aef7f6
tangtao <tangrongtao69@163.com>
2022-06-29 09:21:40 +0800

    1.课程内容设计与基本代码生成

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,4 +1,13 @@
 
+-- 课程内容
+drop table if exists `course_content`;
+create table `course_content` (
+  `id` char(8) not null default '' comment '课程id',
+  `content` mediumtext not null comment '课程内容',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='课程内容';
+
+
 -- 分类
 drop table if exists `category`;
 create table `category` (
