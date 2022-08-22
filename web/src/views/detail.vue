ee5908c8cdf16d3a4d3a06835392815ceefa8671
tangtao <tangrongtao69@163.com>
2022-08-23 01:31:57 +0800

    1.课程详情页面开发：增加大章小节显示 使用d-none d-sm-inline 组合：超小屏隐藏，其他显示v-for的第二变量是索引号

--- web/src/views/detail.vue
+++ web/src/views/detail.vue
@@ -44,11 +44,32 @@
                                 <div class="tab-pane active" id="info" v-html="course.content">
                                 </div>
                                 <div class="tab-pane" id="chapter">
+                                    <div v-for="chapter in chapters" class="chapter">
+                                        <div class="chapter-chapter">
+                                            <span class="folded-button">{{chapter.name}}</span>
+                                        </div>
+                                        <div>
+                                            <table class="table table-striped">
+                                                <tr v-for="(s, j) in chapter.sections" class="chapter-section-tr">
+                                                    <td class="col-sm-8 col-xs-12">
+                                                        <div class="section-title">
+                                                            <i class="fa fa-video-camera d-none d-sm-inline"></i>&nbsp;&nbsp;
+                                                            <span class="d-none d-sm-inline">第{{j+1}}节&nbsp;&nbsp;</span>
+                                                            {{s.title}}
+                                                            <span v-show="s.charge !== SECTION_CHARGE.CHARGE.key" class="badge badge-primary hidden-xs">免费</span>
+                                                        </div>
+                                                    </td>
+                                                    <td class="col-sm-1 text-right">
+                                                        <span class="badge badge-primary">{{s.time | formatSecond}}</span>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
                         </div>
-
                         <!-- 讲师信息 -->
                         <div class="col-md-3">
                             <div class="card" style="width: 18rem;">
@@ -80,9 +101,10 @@
                 id:"",
                 course:{},
                 teacher:{},
-                chapter:[],
+                chapters:[],
                 sections:[],
-                COURSE_LEVEL:COURSE_LEVEL
+                COURSE_LEVEL:COURSE_LEVEL,
+                SECTION_CHARGE: SECTION_CHARGE
             }
 
         },
@@ -101,11 +123,23 @@
                            let res = resp.data;
                            this.course = res.content;
                            this.teacher = this.course.teacher ||{};
-                           this.chapter = this.course.chapter ||{};
+                           this.chapters = this.course.chapters ||{};
                            this.sections = this.course.sections ||{};
-                });
 
-                console.log("信息：",this.chapters)
+
+                    for (let i = 0; i < this.chapter.length; i++) {
+                        let c = this.chapter[i];
+                        c.sections=[];
+                        for (let j = 0; j < this.sections.length; j++) {
+                            let s = this.sections[j];
+
+                            if (c.id === s.chapterId ){
+                                c.sections.push(s)
+                            }
+                        }
+
+                    }
+                });
 
             }
 
@@ -116,6 +150,7 @@
 </script>
 
 <style scoped>
+    /* 课程信息 */
     .course-head {
     }
     .course-head h1 {
@@ -129,18 +164,53 @@
         font-size: 1rem;
         color: #555
     }
-
     .course-head a {
     }
-
     .course-head-price {
         font-size: 2rem;
     }
-
     @media (max-width: 700px) {
         .course-head h1 {
             font-size: 1.5rem;
         }
     }
 
+    /* 章节列表 */
+    .chapter {
+        padding-bottom: 1.25rem;
+    }
+    .chapter-chapter {
+        font-size: 1.25rem;
+        padding: 1.25rem;
+        background-color: #23527c;
+        color: white;
+    }
+    .chapter-section-tr {
+        font-size: 1rem;
+    }
+    .chapter-section-tr td{
+        padding: 1rem 1.25rem;
+        vertical-align: middle;
+    }
+    /*鼠标手势*/
+    .chapter-section-tr td .section-title{
+        color: #555;
+    }
+    .chapter-section-tr td .section-title:hover{
+        color: #23527c;
+        font-weight: bolder;
+        cursor: pointer;
+    }
+    /*行头小图标*/
+    .chapter-section-tr td .section-title i{
+        color: #2a6496;
+    }
+    @media (max-width: 700px) {
+        .chapter-chapter {
+            font-size: 1.2rem;
+        }
+        .chapter-section-tr {
+            font-size: 0.9rem;
+        }
+    }
 </style>
