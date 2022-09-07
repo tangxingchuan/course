e9f8f7df0023e61b650d450a76293ea2557004b1
tangtao <tangrongtao69@163.com>
2022-09-07 23:02:59 +0800

    1.将所有模块的添加Nacos的配置中心 2.添加bootstrap.properties文件，他的优先级比application.properties高

--- gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
