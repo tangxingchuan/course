41b76a5c0a8473fc9e70f3567f0992d8e3cff61f
tangtao <tangrongtao69@163.com>
2022-09-07 16:35:12 +0800

    1.修改publicPath , 本地开发使用'/' 、 生产环境使用'./'

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
     lintOnSave:false /*关闭语法检查*/,
-    publicPath: './'
+    publicPath: process.env.NODE_ENV === 'production'
+    ?'./'
+    :'/'
 
 })
